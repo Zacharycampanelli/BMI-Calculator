91a77ab7950e68f9277fb85195285ff9fc614559
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2023-05-31 22:44:19 -0400

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Body Mass Index Calculator</h1>
+      <p>Better understand your weight in relation to your height using our body mass index (BM) calculator. While BMI is not the sole determinant of a healthy weight, it offers a valuable starting point to evaluate your overall health and well-being.</p>
+    </div>
+  );
+};
+
+export default Header;
